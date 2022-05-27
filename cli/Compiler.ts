4ceb3a821ceab5d2fbc8a97ff8f21b9925cfac09
dev.dmtrllv <dev.dmtrllv@gmail.com>
2022-05-27 15:34:38 +0200

    updated config logic

--- cli/Compiler.ts
+++ cli/Compiler.ts
@@ -6,43 +6,19 @@ import ManifestPlugin from "./ManifestPlugin";
 
 import path from "path";
 import { readdirSync, rmSync, unlinkSync, writeFileSync } from "fs";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export class Compiler
 {
-	private static createConfig(basePath: string, config: CompilerConfig, isServer: boolean): webpack.Configuration
+	private static createBaseConfig(name: string, dev: boolean): MarkRequired<webpack.Configuration, "module" | "resolve" | "plugins" | "experiments" | "mode" | "name" | "devtool" | "output">
 	{
-		const tsLoaders: any[] = [
-			{
-				loader: "ts-loader",
-				options: {
-					transpileOnly: true,
-					experimentalWatchApi: true,
-				}
-			}
-		];
-
-		if (isServer)
-			tsLoaders.push({
-				loader: path.resolve(__dirname, "../dynamicImportPathLoader.js"),
-				options: {
-					root: basePath,
-					dev: config.dev
-				}
-			});
-
-		const c: webpack.Configuration = {
-			mode: config.dev ? "development" : "production",
-			name: config.name,
-			devtool: config.dev ? "inline-source-map" : false,
-			output: {
-				clean: false,
-				filename: isServer ? `[name].js` : `js/[name].bundle.js`,
-				chunkFilename: isServer ? `[id].[fullhash].js` : `js/[id].[fullhash].js`,
-				path: isServer ? config.outPath : `${config.outPath}/public`,
-				publicPath: "/",
-			},
+		return {
+			mode: dev ? "development" : "production",
+			name: name,
+			output: {},
+			devtool: dev ? "inline-source-map" : false,
 			resolve: {
-				modules: ["node_modules"],
+				modules: ["node_modules"], // needed ???
 				extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
 				fallback: {
 					"fs": false,
@@ -56,93 +32,173 @@ export class Compiler
 			},
 			plugins: [],
 			module: {
-				rules: [
-					{
-						test: /\.(ts|js)x?$/,
-						exclude: /node_modules/,
-						use: tsLoaders,
-					},
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						use: ["source-map-loader"],
-						enforce: "pre"
-					}
-				],
+				rules: []
 			},
 			experiments: {
 				topLevelAwait: true
-			},
-			externals: isServer ? [
-				nodeExternals()
-			] : {}
+			}
+		};
+	}
+
+	private static createClientConfig(basePath: string, { outPath, dev = false, name, entries }: CompilerConfig)
+	{
+		const config = this.createBaseConfig(name, dev);
+
+		config.output = {
+			clean: false,
+			filename: "js/[name].bundle.js",
+			chunkFilename: "js/[id].[fullhash].js",
+			path: `${outPath}/public`,
+			publicPath: "/",
 		};
 
-		if (isServer)
-		{
-			c.plugins = [
-				new DefinePlugin({
-					env: JSON.stringify({
-						isDev: config.dev,
-						appEntries: Object.keys(config.entries),
-						isClient: false,
-						isServer: true
-					})
-				}),
-				new ForkTsCheckerWebpackPlugin({
-					typescript: {
-						mode: "write-references"
-					}
-				}),
-				new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-			];
-			c.target = "node";
-			c.externalsPresets = { node: true };
-			c.entry = {
-				"index": config.serverEntry || path.resolve(__dirname, "../server-entry.ts"),
-				...config.entries
-			};
-			c.output!.library = "App";
-			c.output!.libraryTarget = "commonjs";
-		}
-		else
-		{
-			c.output!.clean = true;
-			c.plugins = [
-				new DefinePlugin({
-					env: JSON.stringify({
-						isDev: config.dev,
-						isClient: true,
-						isServer: false
-					})
-				}),
-				new ForkTsCheckerWebpackPlugin({
-					typescript: {
-						mode: "write-references"
-					}
-				}),
-				new ManifestPlugin(path.resolve(config.outPath, ".."), (manifest) => {
-					writeFileSync(path.resolve(config.outPath, "manifest.json"), JSON.stringify(manifest), "utf-8");
+		config.plugins = [
+			new MiniCssExtractPlugin({
+				filename: `css/[name].bundle.css`,
+				chunkFilename: `css/[id].chunk.css`,
+				ignoreOrder: false
+			}),
+			new DefinePlugin({
+				env: JSON.stringify({
+					isDev: dev,
+					isClient: true,
+					isServer: false
 				})
-			]
-			c.entry = config.entries;
-			c.optimization = {
-				runtimeChunk: "single",
-				splitChunks: {
-					chunks: "all",
-					cacheGroups: {
-						vendor: {
-							test: /[\\/]node_modules[\\/]/,
-							name: "vendors",
-							chunks: "all",
-							enforce: true,
-						}
+			}),
+			new ForkTsCheckerWebpackPlugin({
+				typescript: {
+					mode: "write-references"
+				}
+			}),
+			new ManifestPlugin(basePath, (manifest) => writeFileSync(path.resolve(outPath, "manifest.json"), JSON.stringify(manifest), "utf-8")),
+		];
+
+		config.entry = entries;
+		config.optimization = {
+			runtimeChunk: "single",
+			splitChunks: {
+				chunks: "all",
+				cacheGroups: {
+					vendor: {
+						test: /[\\/]node_modules[\\/]/,
+						name: "vendors",
+						chunks: "all",
+						enforce: true,
 					}
 				}
-			};
-		}
+			}
+		};
 
-		return c;
+		config.module.rules = [
+			{
+				loader: "ts-loader",
+				options: {
+					transpileOnly: true,
+					experimentalWatchApi: true,
+				}
+			},
+			{
+				loader: path.resolve(__dirname, "../dynamicImportPathLoader.js"),
+				options: {
+					root: basePath,
+					dev
+				}
+			},
+			{
+				test: /\.(glsl|vert|frag)$/i,
+				use: "raw-loader",
+			},
+			{
+				test: /\.s?(c|a)ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"sass-loader",
+				],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|ico|webp)$/i,
+				use: {
+					loader: "url-loader",
+					options: {
+						fallback: "file-loader",
+						limit: 40000,
+						name: "images/[name].[ext]",
+					},
+				},
+			}
+		];
+		return config;
+	}
+
+	private static createServerConfig(basePath: string, { name, dev = false, entries, serverEntry, outPath }: CompilerConfig)
+	{
+		const config = this.createBaseConfig(name, dev);
+		config.externals = [nodeExternals()];
+		config.target = "node";
+		config.externalsPresets = { node: true };
+		config.entry = {
+			index: serverEntry || path.resolve(__dirname, "../server-entry.ts"),
+			...entries,
+		};
+		config.output = {
+			clean: false,
+			filename: "[name].js",
+			chunkFilename: "[id].[fullhash].js",
+			path: `${outPath}`,
+			publicPath: "/",
+		};
+		config.output!.library = "App";
+		config.output!.libraryTarget = "commonjs";
+		config.plugins = [
+			new DefinePlugin({
+				env: JSON.stringify({
+					isDev: dev,
+					appEntries: Object.keys(entries),
+					isClient: false,
+					isServer: true
+				})
+			}),
+			new ForkTsCheckerWebpackPlugin({
+				typescript: {
+					mode: "write-references"
+				}
+			}),
+			new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+		];
+		config.module.rules = [
+			{
+				loader: "ts-loader",
+				options: {
+					transpileOnly: true,
+					experimentalWatchApi: true,
+				}
+			},
+			{
+				loader: path.resolve(__dirname, "../dynamicImportPathLoader.js"),
+				options: {
+					root: basePath,
+					dev
+				}
+			},
+			{
+				test: /\.(glsl|vert|frag|s?(c|a)ss)$/i,
+				use: "ignore-loader",
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg|ico|webp)$/i,
+				use: {
+					loader: "url-loader",
+					options: {
+						fallback: "file-loader",
+						limit: 40000,
+						name: "images/[name].[ext]",
+					},
+				},
+			}
+		];
+		return config;
 	}
 
 	private readonly clientConfig: webpack.Configuration = {};
@@ -161,8 +217,8 @@ export class Compiler
 	{
 		this.outPath = config.outPath;
 		this.isDev = config.dev || false;
-		this.clientConfig = Compiler.createConfig(basePath, config, false);
-		this.serverConfig = Compiler.createConfig(basePath, config, true);
+		this.clientConfig = Compiler.createClientConfig(basePath, config);
+		this.serverConfig = Compiler.createServerConfig(basePath, config);
 	}
 
 	public updateName(name: string)
@@ -221,7 +277,7 @@ export class Compiler
 				{
 					readdirSync(this.serverConfig.output!.path!).forEach((name) => 
 					{
-						if(name.endsWith(".js"))
+						if (name.endsWith(".js"))
 							unlinkSync(path.resolve(this.serverConfig.output!.path!, name));
 					});
 					cb();
@@ -240,7 +296,7 @@ export class Compiler
 		else
 		{
 			this.clientConfig.mode = this.serverConfig.mode = "development";
-			(this.serverConfig.module!.rules![0] as any)!.use[1].options.dev = true;
+			(this.serverConfig.module!.rules![1] as any).options.dev = true;
 			startWatching();
 		}
 	}
@@ -277,3 +333,7 @@ type CompilerConfig = {
 	outPath: string;
 	serverEntry?: string | undefined;
 };
+
+type A = MarkRequired<CompilerConfig, "dev", true>;
+
+type MarkRequired<T, K extends keyof T, MakeRestPartial extends boolean = false> = Required<Pick<T, K>> & (MakeRestPartial extends true ? Partial<Omit<T, K>> : Omit<T, K>);
